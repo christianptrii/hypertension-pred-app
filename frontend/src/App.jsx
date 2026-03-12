b6c0ca59f038e012b900594921fd5c70fa9766ff
Christian Putri <christianptrii@gmail.com>
2026-03-13 01:47:42 +0800

    initiate frontend with react js and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-600">
+      <div className="bg-white p-10 rounded-2xl shadow-2xl text-center">
+        <h1 className="text-3xl font-bold text-blue-600">Tailwind Berhasil! 🚀</h1>
+        <p className="text-gray-500 mt-2">Sekarang kita bisa lanjut buat tampilan websitenya.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
